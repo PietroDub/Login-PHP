63ec854c4e842a527520960eff5da5d3673c79a2
Pietro <pietroti453@gmail.com>
2025-09-24 18:39:48 -0300

    mudando para index.php e começando a ligação com banco de dados

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Full-Stack Login & Register</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+     <div class="container">
+        <div class="form-box active" id="login-form">
+            <form action="login_register.php" method="post">
+                <h2>Login</h2>
+                <input type="email" name="email" placeholder="Email" id="" required>
+                <input type="password" name="password" placeholder="Password" required>
+                <button type="submit" name="login">Login</button>
+                <p>Dont have an account? <a href="" onclick="ShowForm('register-form')">Register</a></p>
+            </form>
+        </div>
+
+         <div class="form-box" id="register-form">
+            <form action="login_register.php" method="post">
+                <h2>Register</h2>
+                <input type="text" name="name" placeholder="Name" id="" required>
+                <input type="email" name="email" placeholder="Email" id="" required>
+                <input type="password" name="password" placeholder="Password" required>
+                <select name="role" required>
+                    <option value="">--Select Role--</option>
+                    <option value="user">User</option>
+                    <option value="admin">Admin</option>
+                </select>
+                <button type="submit" name="Register">Register</button>
+                <p>Already have an account?<a href="" onclick="ShowForm('login-form')">Login</a></p>
+            </form>
+        </div>
+     </div>
+     <script src="script.js"></script>
+</body>
+</html>
